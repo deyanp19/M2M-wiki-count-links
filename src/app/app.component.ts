5635935c9f28bf7a4735950dcd7f42a3429b1a6b
deyan petrov <deyanp19@hotmail.com>
2024-09-30 18:23:55 +0300

    bootstrap implementatio not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule, NgbModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'M2M-Wiki-count-links';
+
+  constructor(private modalService: NgbModule){}
+  onSubmit(){
+    console.log('submitting');
+    
+  }
+  public open(modal: any): void {
+    this.modalService;
+  }
+
 }
