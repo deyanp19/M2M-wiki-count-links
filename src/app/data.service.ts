c2a9ede7ad10b6b97f60bcc65f15c6b5216a35b9
deyan petrov <deyanp19@hotmail.com>
2024-10-01 17:07:03 +0300

    changes in the search parameters static search

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -12,6 +12,6 @@ export class DataService {
   constructor(private http: HttpClient) {}
 
   getData(): Observable<any>{
-     return this.http.get<any>('https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch=%27New_England_Patriots%27');
+     return this.http.get<any>('https://en.wikipedia.org/w/api.php?action=opensearch&format=json&gsrnamespace=0&gsrlimit=5&search=%27kevin%20bakon%27');
   }
 }
